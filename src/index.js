1cc383aaa13373fa58401aa92570d969f5ada9af
syadav <syadav@gozego.com>
2022-08-10 18:13:02 +0530

    Initial commit along with mongoDb setup

---
+++ src/index.js
@@ -0,0 +1,26 @@
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const mongoUri = 'mongodb+srv://syada13:g2f9IdfxvjvJY1yk@cluster0.4yayjuw.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+   console.log('Connected to mongodb instance'); 
+});
+
+mongoose.connection.on('error',(err) => {
+    console.error('Error connecting to mongodb', err);
+
+});
+
+app.get('/', (req,res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () =>{
+    console.log('Listening on port 3000');
+
+});
